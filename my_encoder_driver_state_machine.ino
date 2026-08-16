1f85c22cb67916e3244b02e961a0009b1139c378
fgrothaus <felixgrothaus1@gmail.com>
2026-08-16 19:58:31 +0200

    add comments for doc

--- my_encoder_driver_state_machine.ino
+++ my_encoder_driver_state_machine.ino
@@ -1,17 +1,26 @@
 #include "Arduino.h"
-#include "soc/gpio_reg.h" // HAL-Header für direkten Registerzugriff
+#include "soc/gpio_reg.h" // HAL-Header für direkten Registerzugriff (schneller)
 
-const int pinCLK = 18;
-const int pinDT  = 19;
-const int pinSW  = 32;
+const int pinCLK = 18; // Taktgeber
+const int pinDT  = 19; // Links-/Rechtsdrehung (Data)
+const int pinSW  = 32; // Button-Push
 
+// Alle vier Werte sind als volatile markiert, damit der Compiler weiß, dass er immer im RAM nachschauen muss, ob sich ein Wert geändert hat.
+// Das liegt daran, dass die folgenden Variablen nicht im Hauptprogramm geändert werden, sondern nur über die ISRs, die allerdings außerhalb der Sicht
+// des Compilers sind. ISRs sind vollständig entkoppelt vom Programm und werden über das OS(beim Ardunino gibt es kein OS)/Interrupt-Vektortabelle
+// ausgeführt. Da sich im Hauptprogramm die Variablen nicht ändern, würde der Compiler die bestehenden Werte im CPU-Register cachen
+// und nicht erneut im RAM gucken, ob sich was geändert hat. Mit volatile weiß der Compiler, dass die Werte volatil sind und über extern geändert werden könnten.
 volatile int drehZaehler = 0;
 volatile uint8_t alterZustand = 0; // Speichert die letzten Pin-Zustände
 volatile unsigned long letzteFlankeSW = 0;
 volatile bool buttonGedrueckt = false;
 
-// Lookup-Tabelle für die 16 möglichen Zustandsübergänge (Gray Code)
+// Lookup-Tabelle für die 16 möglichen Zustandsübergänge
 // 0 = Ungültig/Prellen, 1 = Rechts, -1 = Links
+// Die encoderTabelle stellt den Zustandsautomaten dar.
+// Der Encoder kann bei jeder Drehung folgende Zustände durchlaufen (Gray Code):
+// Rechts: 11-10-00-01 (jeder Zustandswechsel wird mit +1 gewertet. Also 11 -> 10 =>  Index = (3 << 2)|2 = 14)
+// Links: 11-01-00-10
 const int8_t encoderTabelle[] = {
    0,  1, -1,  0,
   -1,  0,  0,  1,
@@ -20,20 +29,28 @@ const int8_t encoderTabelle[] = {
 };
 
 // Diese ISR läuft blitzschnell bei JEDER Änderung an CLK oder DT
+// Pro Raste werden alle vier Zustände durchlaufen. D.h. pro Raste wird 4x die encoderISR aufgerufen
 void IRAM_ATTR encoderISR() {
-  // Liest den gesamten GPIO-Eingangs-Bus (Pins 0-31) in einem einzigen Maschinencode-Befehl aus:
+  // REG_READ liest das gesamte 32-Bit-Register (Adresse 0x3FF4403C) im GPIO-Controller an der Andresse GPIO_IN_REG, in der die Zustände von Pin 0-31 stehen (32 Bit)
   uint32_t gpio_in = REG_READ(GPIO_IN_REG);
   
-  // Wir holen uns die Bits für Pin 18 (CLK) und Pin 19 (DT)
+  // Aus dem Snapshot werden nun die Zustände von CLK und DT extrahiert und nebeneinander in eine Byte Variable gepackt
+  // Es kommen dadurch vier elektrische Zustände in Frage:
+  // 000000 00 (0)
+  // 000000 01 (1)
+  // 000000 10 (2)
+  // 000000 11 (3)
+  // (gpio_in >> pinCLK) & 0x01) << 1 = 00000010; ((gpio_in >> pinDT) & 0x01) = 00000001 => 00000010 | 00000001 = 00000011 z.B.
   uint8_t neuerZustand = ((gpio_in >> pinCLK) & 0x01) << 1 | ((gpio_in >> pinDT) & 0x01);
 
-  // 2. Alten und neuen Zustand verknüpfen (4-Bit-Index für die Tabelle)
-  uint8_t index = ((alterZustand & 0x03) << 2) | neuerZustand;
+  // Die Berechnung des Index' stellt den Zustandswechsel dar, anhand dem inerpretiert wird, ob es sich um eine Links- oder Rechtsdrehung handelt.
+  // (encoderTabelle hat 16 Werte => 0x0 bis 0xF Indizes)
+  uint8_t index = ((alterZustand & 0x03) << 2) | neuerZustand; // z.B. (00000010 & 00000011) << 2 | 00000011 => 00001000 | 00000011 = 00001011 = 11
 
-  // 3. Tabelle abfragen & addieren
+  // Roh-Steps. Bei Rechtsdrehung Index 1, 7, 8 und 14 => jeweils +1 und bei Linksdrehung 2, 4, 11 ,13
   drehZaehler += encoderTabelle[index];
 
-  // 4. Zustand speichern
+  // Zustand speichern, um innerhalb der Raste den Zustandswechsel bestimmen zu können.
   alterZustand = neuerZustand;
 }
 
@@ -71,17 +88,17 @@ void loop() {
   buttonGedrueckt = false;
   interrupts();
 
-  // Da ein mechanisches Rasten meist 4 Zustandsschritte entspricht, 
-  // teilen wir durch 4, um exakt 1 Zähler pro Raste zu bekommen!
-  int echeRasten = aktuellerStand / 4; 
+  // Da ein mechanisches Rasten 4 Zustandsschritten entspricht, 
+  // muss durch 4 geteilt werden, um pro Raste genau um 1 hochzuzählen.
+  int echteRasten = aktuellerStand / 4; 
 
-  if (echeRasten != letzterStand || wasReset) {
+  if (echteRasten != letzterStand || wasReset) {
     if (wasReset) {
       Serial.println("-> RESET! Zaehler auf 0.");
       letzterStand = 0;
     } else {
-      Serial.printf("Raste: %d (Roh-Steps: %d)\n", echeRasten, aktuellerStand);
-      letzterStand = echeRasten;
+      Serial.printf("Raste: %d (Roh-Steps: %d)\n", echteRasten, aktuellerStand);
+      letzterStand = echteRasten;
     }
   }
 
